0e9d3de8cca23c4d87bba72fc453fb13de7ae270
BinaryJourney <71597536+BinaryJourney@users.noreply.github.com>
2022-04-04 16:07:57 +0200

    Initial commit

---
+++ src/main/java/isep/webtechno/placeholder/WebPlaceholderApplication.java
@@ -0,0 +1,13 @@
+package isep.webtechno.placeholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPlaceholderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPlaceholderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/isep/webtechno/placeholder/WebPlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.webtechno.placeholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPlaceholderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
